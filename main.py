6d4a56a1728270f520d3645cd9909d2414436401
hargonger <kevin.gong@harbingermotors.com>
2025-03-06 11:08:47 -0800

    modified power again, prev barely reached 30psi

--- main.py
+++ main.py
@@ -890,13 +890,13 @@ class PumpControlApp(QMainWindow):
             time.sleep(2)
 
         while self._test_active and self.pressure_cycle_count < self.pressure_num_cycles:
-            self._cantroller.set_bcm_power(70)
+            self._cantroller.set_bcm_power(75)
             self._cantroller.set_ptn_power(75)
-            time.sleep(4.27)
+            time.sleep(4)
 
             self._cantroller.set_bcm_power(0)
             self._cantroller.set_ptn_power(0)
-            time.sleep(1)  
+            time.sleep(1.27)  
 
             #print(f"Julabo temp: {self._julabo.get_temperature()}") # Debug statement
 
